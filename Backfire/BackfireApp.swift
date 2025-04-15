adefec8faffe185d1388051e366e04bf34ac7edd
Adil Rahmani <didilusse@gmail.com>
2025-04-15 16:57:54 -0400

    Initial Commit

---
+++ Backfire/BackfireApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackfireApp.swift
+//  Backfire
+//
+//  Created by Adil Rahmani on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct BackfireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
